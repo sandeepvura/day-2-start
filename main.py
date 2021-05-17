25fece3a2c9411691ffdab984cf437303f2e4f7c
sandeepvura <>
2021-05-17 10:55:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Data Types
+
+#Strings
+print("Hello"[4])
+
+#Integers
+print(123 + 345)
+
+#Float 
+3.1456
+
+#Boolean 
+True 
+False
+
+#Find the data type of the variable. 
+mystery = 734_529.678
+type(mystery)
